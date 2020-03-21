ea0e0003fc14a02f9f0ffafd2e3260a0b00a87c2
Patryk Pacyna <pacynek93@gmail.com>
2020-03-21 16:26:37 +0100

    task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i = 0; i < 50; i++) { 
+      for (let i = 0; i < bracketsConfig.length; i++) {
+        const addIt =  bracketsConfig[i][0] + bracketsConfig[i][1];
+        str = str.split(addIt).join('');
+      }
+  }
+  if (str == false) {
+    return true;
+  } else {
+    return false;
+  }
 }
